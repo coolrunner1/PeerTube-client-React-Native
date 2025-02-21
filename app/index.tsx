b5f6e579977ffb6f1321a12b8b52c72a376bdd80
coolrunner1 <waurttttt@gmail.com>
2025-02-21 20:42:28 +0300

    Testing

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect, useState } from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 export default function Index() {
+  const [visible, setVisible] = useState<boolean>(false)
+
+  const clickTest = () => {
+    setVisible(!visible);
+  }
+
   return (
     <View
       style={{
@@ -10,10 +17,11 @@ export default function Index() {
         backgroundColor: "#ff76gab"
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <View style={styles.button}>
+      <Text>Edit app/index.tsx to edit this dfdfscresdfden.</Text>
+      <View style={styles.button} onTouchStart={clickTest} onPointerDown={clickTest}>
         <Text style={styles.buttonText}>Button</Text>
       </View>
+      {visible && <Text style={styles.testText}>Test</Text>}
     </View>
   );
 }
@@ -22,10 +30,15 @@ const styles = StyleSheet.create({
   button: {
     padding: 10,
     backgroundColor: "purple",
-    borderRadius: 10,
+    borderRadius: 10
   },
   buttonText: {
     color: 'white',
     fontWeight: "bold"
+  },
+  testText: {
+    fontSize: 90,
+    fontWeight: "bold",
+    color: "pink"
   }
 })
